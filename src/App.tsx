f0c6a318c49bd4d9a48a3164f3744e0d0fd1edd2
Raul Battistini <raulbattistini.3@gmail.com>
2023-01-21 18:02:23 -0300

    fix: app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
-import { RoutesList } from './routes'
+import React from "react";
+import { RoutesList } from "./routes";
 
 export const App = () => {
-  return (
-    <div><RoutesList/></div>
-  )
-}
+  return <RoutesList />;
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,30 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { FAQ } from './pages/FAQ'
-import { Home } from './pages/Home'
-import { Cardapio } from './pages/Menu'
-import { Menu } from './pages/MenuRedir'
-import { NotFound } from './pages/NotFound'
-import { CakePage } from './pages/CakePage'
-import { JellyPage } from './pages/JellyPage'
-import { TYP } from './pages/TYP'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FAQ } from "./pages/FAQ";
+import { Home } from "./pages/Home";
+import { Cardapio } from "./pages/Menu";
+import { Menu } from "./pages/MenuRedir";
+import { NotFound } from "./pages/NotFound";
+import { CakePage } from "./pages/CakePage";
+import { JellyPage } from "./pages/JellyPage";
+import { TYP } from "./pages/TYP";
+import { Navbar } from "./components/navbar";
+import { Footer } from "./components/footer";
 
 export const RoutesList = () => {
   return (
     <Router>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/cardapio"  element={<Cardapio/>} />
-            <Route path="/bolos/:link"  element={<CakePage/>} />
-            <Route path="/geleias/:link"  element={<JellyPage/>} />
-            <Route path="/menu" element={<Menu/>}/>
-            <Route path="/FAQ"  element={<FAQ/>} />
-            <Route path="/thank-you"  element={<TYP/>} />
-            <Route path="*" element={<NotFound/>}/>
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cardapio" element={<Cardapio />} />
+        {/* <Route path="/bolos/:link"  element={<CakePage imagePath={''}/>} /> */}
+        {/* <Route path="/geleias/:link"  element={<JellyPage/>} /> */}
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/FAQ" element={<FAQ />} />
+        {/* <Route path="/thank-you"  element={<TYP/>} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
     </Router>
-  )
-}
+  );
+};
