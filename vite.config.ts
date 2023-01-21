4f814a29dfd3b4f765d9d736892f4d726aee394b
Raul Battistini <raulbattistini.3@gmail.com>
2023-01-21 18:02:41 -0300

    upd: config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+      manifest: {
+        name: "vite-react-ts-100",
+        short_name: "vite-react-ts-100",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        lang: "pt-br",
+        scope: "/",
+        icons: [
+          {
+            src: "/icons/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/icons/manifest-icon-192.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+      },
+    }),
+  ],
+});
